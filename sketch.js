5727d609b68fc5e306766a2b55d8a036c784c8d6
brickdown <42557081+brickdown@users.noreply.github.com>
2018-08-21 21:00:48 +0200

    ver. 0.2

--- sketch.js
+++ sketch.js
@@ -9,49 +9,53 @@ function buttonClicked(){
         brickBegin + textarea.value.substring  (textarea.selectionStart, textarea.selectionEnd) + brickEnd +
         textarea.value.substring (textarea.selectionEnd);
     textarea.value = newText;
+    preview();
 }
 
 function buttonClickedDiv(){
-  textarea = document.getElementById("textArea1");
+  var textarea = document.getElementById("textArea1");
   var targ = event.target || event.srcElement;
 
-  brickBegin = "<div class = " + targ.textContent + ">";
-  brickEnd = "</div>";
+  var brickBegin = "<div class = " + targ.textContent + ">";
+  var brickEnd = "</div>";
 
   var newText = textarea.value.substring (0, textarea.selectionStart) + 
       brickBegin + textarea.value.substring  (textarea.selectionStart, textarea.selectionEnd) + brickEnd +
       textarea.value.substring (textarea.selectionEnd);
   textarea.value = newText;
+  preview();
 }
 
 function buttonClickedImg(){
-  textarea = document.getElementById("textArea1");
+  var textarea = document.getElementById("textArea1");
   var targ = event.target || event.srcElement;
 
-  brickBegin = "<img src= \"";
-  brickEnd =  "\" />";
+  var brickBegin = "<img src= \"";
+  var brickEnd =  "\" />";
 
   var newText = textarea.value.substring (0, textarea.selectionStart) + 
       brickBegin + textarea.value.substring  (textarea.selectionStart, textarea.selectionEnd) + brickEnd
       textarea.value.substring (textarea.selectionEnd);
   textarea.value = newText;
+  preview();
 }
 
 function buttonClickedLink(){
-  textarea = document.getElementById("textArea1");
+  var textarea = document.getElementById("textArea1");
   var targ = event.target || event.srcElement;
 
-  brickBegin = "<a href= \"";
-  brickEnd =  "\"> </a>";
+  var brickBegin = "<a href= \"";
+  var brickEnd =  "\"> </a>";
 
   var newText = textarea.value.substring (0, textarea.selectionStart) + 
       brickBegin + textarea.value.substring  (textarea.selectionStart, textarea.selectionEnd) + brickEnd
       textarea.value.substring (textarea.selectionEnd);
   textarea.value = newText;
+  preview();
 }
 
 function buttonClickedSingle(){
-  textarea = document.getElementById("textArea1");
+  var textarea = document.getElementById("textArea1");
   var targ = event.target || event.srcElement;
 
   brickBegin = "<" + targ.textContent + ">";
@@ -60,6 +64,7 @@ function buttonClickedSingle(){
       brickBegin + textarea.value.substring  (textarea.selectionStart, textarea.selectionEnd) +
       textarea.value.substring (textarea.selectionEnd);
   textarea.value = newText;
+  preview();
 }
 
 function copyToClipboard(){
@@ -73,27 +78,25 @@ function copyToClipboard(){
         window.clipboardData.setData('Text', text);
     }
 }
-window.addEventListener('copy', copy);
-document.execCommand('copy');
-window.removeEventListener('copy', copy);
-  alert("Content copied to clipboard!");
+    window.addEventListener('copy', copy);
+    document.execCommand('copy');
+    window.removeEventListener('copy', copy);
+    alert("Content copied to clipboard!");
 }
 
 function preview(){
-   textarea1 = document.getElementById("textArea1").value;
-   // viewHtml = document.getElementByI("textArea2");
-    //document.
-   // viewHtml.open();
-  // viewHtml.write(textarea1);
-    //viewHtml.close();
-    //viewHtml.innerHTML = textarea1.value;
-    //var ifrm = document.getElementById("textArea2");
-    //ifrm.innerHTML = textarea1;
+    var textarea1 = document.getElementById("textArea1").value;
 
     var iframe = document.getElementById("textArea2");
-iframe = iframe.contentWindow || ( iframe.contentDocument.document || iframe.contentDocument);
+    iframe = iframe.contentWindow || ( iframe.contentDocument.document || iframe.contentDocument);
 
-iframe.document.open();
-iframe.document.write(textarea1);
-iframe.document.close();
+    iframe.document.open();
+    iframe.document.write(textarea1);
+    iframe.document.close();
 }
+
+function myConfirmation() {
+    return "Make sure that you copied content to clipboard, all content will be lost after exit";
+}
+
+window.onbeforeunload = myConfirmation;
