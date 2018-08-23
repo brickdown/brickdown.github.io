99cf75de59564b691c0d577f546b99aebfb8b819
brickdown <42557081+brickdown@users.noreply.github.com>
2018-08-23 15:05:49 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,77 +1,127 @@
-function buttonClicked(){
-    var textarea = document.getElementById("textArea1");
-    var targ = event.target || event.srcElement;
-
-    var brickBegin = "<" + targ.textContent + ">";
-    var brickEnd = "</" + targ.textContent + ">";
-
-    var newText = textarea.value.substring (0, textarea.selectionStart) + 
-        brickBegin + textarea.value.substring  (textarea.selectionStart, textarea.selectionEnd) + brickEnd +
-        textarea.value.substring (textarea.selectionEnd);
-    textarea.value = newText;
-    preview();
+function buttonBold(){
+   iframe.contentDocument.execCommand("bold", false, null);
 }
 
-function buttonClickedDiv(){
-  var textarea = document.getElementById("textArea1");
-  var targ = event.target || event.srcElement;
+function buttonItalic(){
+    iframe.contentDocument.execCommand("italic", false, null);
+ }
 
-  var brickBegin = "<div class = " + targ.textContent + ">";
-  var brickEnd = "</div>";
+ function buttonStrike(){
+    iframe.contentDocument.execCommand("strikeThrough", false, null);
+ }
 
-  var newText = textarea.value.substring (0, textarea.selectionStart) + 
-      brickBegin + textarea.value.substring  (textarea.selectionStart, textarea.selectionEnd) + brickEnd +
-      textarea.value.substring (textarea.selectionEnd);
-  textarea.value = newText;
-  preview();
+function buttonJustify(){
+    if(iframe.contentDocument.getSelection() != ""){
+    iframe.contentDocument.execCommand("insertHTML", false, "<div class = text-justify>" + iframe.contentDocument.getSelection() + "</div>");
+    }
+ }
+
+function buttonCenter(){
+   // if(document.getElementById("center").value=="OFF"){
+       // if(iframe.contentDocument.getSelection() != ""){
+            iframe.contentDocument.execCommand("removeFormat", false, null); 
+            iframe.contentDocument.execCommand("insertHTML", false,  "<center>" + iframe.contentDocument.getSelection() + "</center>"); 
+            //iframe.contentDocument.execCommand("formatBlock", false, "p");
+         //   }
+      //  document.getElementById("center").value="ON";
+   // }
+     
+     //  else if(document.getElementById("center").value=="ON"){
+     //   iframe.contentDocument.execCommand("removeFormat", false, null); 
+       // document.getElementById("center").value="OFF";
+   // }
+ }
+ 
+ function buttonLeft(){
+    if(iframe.contentDocument.getSelection() != ""){
+    iframe.contentDocument.execCommand("justifyLeft", false, "<div class = pull-left>" + iframe.contentDocument.getSelection() + "</div>");
+    }
+ }
+
+ function buttonRight(){
+    if(iframe.contentDocument.getSelection() != ""){
+    iframe.contentDocument.execCommand("insertHTML", false, "<div class = pull-right>" + iframe.contentDocument.getSelection() + "</div>");
+
+    }
+ }
+
+ function buttonSub(){
+    iframe.contentDocument.execCommand("subscript", false, null);
+ }
+
+ function buttonSup(){
+    iframe.contentDocument.execCommand("superscript", false, null);
+ }
+
+ function buttonH1(){
+    iframe.contentDocument.execCommand("formatBlock", false, "h1");
+ }
+
+ function buttonH2(){
+    iframe.contentDocument.execCommand("formatBlock", false, "h2");
+ }
+
+ function buttonH3(){
+    iframe.contentDocument.execCommand("formatBlock", false, "h3");
+ }
+
+ function buttonP(){
+    iframe.contentDocument.execCommand("formatBlock", false, "p");
+ }
+
+
+ function buttonHr(){
+    iframe.contentDocument.execCommand("insertHorizontalRule", false, null);
+ }
+
+ function buttonBlockQuote(){
+    iframe.contentDocument.execCommand("formatBlock", false, "blockquote");
+ }
+
+ function buttonCode(){
+    iframe.contentDocument.execCommand("insertHTML", false, "<code>" + iframe.contentDocument.getSelection() + "</code>");
+ }
+
+ function buttonUl2(){
+    iframe.contentDocument.execCommand("formatBlock", false, "ul");
+ }
+
+
+ function buttonUl(){
+    iframe.contentDocument.execCommand("insertOrderedList", false, null);
+ }
+
+ function buttonOl(){
+    iframe.contentDocument.execCommand("insertUnorderedList", false, null);
+ }
+
+ function buttonLink(){
+    var lnk =prompt("Write the URL here","http:\/\/");
+    if(lnk&&lnk!=''&&lnk!="http://"){
+     iframe.contentDocument.execCommand("createLink", false, lnk);
 }
+ }
 
-function buttonClickedImg(){
-  var textarea = document.getElementById("textArea1");
-  var targ = event.target || event.srcElement;
+ function buttonImage(){
+    var lnk =prompt("Write the image URL here or just push it into edit window","http:\/\/");
+    if(lnk!=''&&lnk!="http://"){
+    iframe.contentDocument.execCommand("insertImage", false, "https://steemitimages.com/0x0/" + lnk);
+ }
+ }
 
-  var brickBegin = "<img src= \"";
-  var brickEnd =  "\" />";
+ function buttonTable(){
+    var rows =prompt("ROWS","1");
+    var columns =prompt("COLUMNS","1");
+    iframe.contentDocument.execCommand("insertHTML", false, "<table border=\"1\">" + "<tr><td> </td> <td></td></tr>");
+ }
 
-  var newText = textarea.value.substring (0, textarea.selectionStart) + 
-      brickBegin + textarea.value.substring  (textarea.selectionStart, textarea.selectionEnd) + brickEnd +
-      textarea.value.substring (textarea.selectionEnd);
-  textarea.value = newText;
-  preview();
-}
 
-function buttonClickedLink(){
-  var textarea = document.getElementById("textArea1");
-  var targ = event.target || event.srcElement;
-
-  var brickBegin = "<a href= \"";
-  var brickEnd =  "\"> </a>";
-
-  var newText = textarea.value.substring (0, textarea.selectionStart) + 
-      brickBegin + textarea.value.substring  (textarea.selectionStart, textarea.selectionEnd) + brickEnd +
-      textarea.value.substring (textarea.selectionEnd);
-  textarea.value = newText;
-  preview();
-}
-
-function buttonClickedSingle(){
-  var textarea = document.getElementById("textArea1");
-  var targ = event.target || event.srcElement;
-
-  brickBegin = "<" + targ.textContent + ">";
-
-  var newText = textarea.value.substring (0, textarea.selectionStart) + 
-      brickBegin + textarea.value.substring  (textarea.selectionStart, textarea.selectionEnd) +
-      textarea.value.substring (textarea.selectionEnd);
-  textarea.value = newText;
-  preview();
-}
 
 function copyToClipboard(){
   var copy = function (e) {
     e.preventDefault();
     console.log('copy');
-    var text = document.getElementById("textArea1").value;
+    var text = document.getElementById("textArea2").contentDocument.body.innerHTML;
     if (e.clipboardData) {
         e.clipboardData.setData('text/plain', text);
     } else if (window.clipboardData) {
@@ -84,19 +134,24 @@ function copyToClipboard(){
     alert("Content copied to clipboard!");
 }
 
-function preview(){
-    var textarea1 = document.getElementById("textArea1").value;
-
-    var iframe = document.getElementById("textArea2");
-    iframe = iframe.contentWindow || ( iframe.contentDocument.document || iframe.contentDocument);
-
-    iframe.document.open();
-    iframe.document.write(textarea1);
-    iframe.document.close();
-}
-
 function myConfirmation() {
     return "Make sure that you copied content to clipboard, all content will be lost after exit";
 }
 
 window.onbeforeunload = myConfirmation;
+
+//__________________________________________
+function editableIFrame()
+{
+iframe = 
+document.getElementById("textArea2");
+iframe.contentDocument.designMode = "on";
+var cssLink = document.createElement("link");
+cssLink.href = "style.css";  
+cssLink.rel = "stylesheet";  
+cssLink.type = "text/css";  
+frames["iframe"].contentDocument.body.appendChild(cssLink); 
+}
+window.addEventListener("loader", onload, false);
+
+//________________________
